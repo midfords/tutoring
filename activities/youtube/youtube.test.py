d647d21f30d5415f59099fc3839d227ba6484d40
Sean Midford <seanmidford@outlook.com>
2019-11-03 17:20:17 -0500

    Separated unit tests.

--- activities/youtube/youtube.test.py
+++ activities/youtube/youtube.test.py
@@ -1,9 +1,16 @@
+import sys
 import unittest
+
+sys.path.append('../src')
 from youtube import find_added_items
 from youtube import find_missing_items
 from youtube import find_renamed_items
 
 class TestYoutubeDiff(unittest.TestCase):
+
+    #
+    # Setup
+    #
 
     def setUp(self):
         self.old_items = [
@@ -20,11 +27,20 @@ class TestYoutubeDiff(unittest.TestCase):
             "00000000005": "Item 5"
         }
 
+    #
+    # find_added_items
+    #
+
     def test_added_items(self):
-        expected_result = [
+        expected = [
             ("00000000005", "Item 5")
         ]
-        expected_old = [
+        actual = find_added_items(self.old_items, self.new_items)
+
+        self.assertEqual(expected, actual)
+
+    def test_added_items_master(self):
+        expected = [
             ["", "00000000005", "Item 5"],
             ["", "00000000000", "Item 0"],
             ["", "00000000001", "Item 1"],
@@ -34,17 +50,22 @@ class TestYoutubeDiff(unittest.TestCase):
         ]
         actual = find_added_items(self.old_items, self.new_items)
 
-        self.assertEqual(expected_result, actual)
-        self.assertEqual(expected_old, self.old_items)
+        self.assertEqual(expected, self.old_items)
 
     def test_added_items_empty(self):
-        expected_result = [
+        expected = [
             ("00000000000", "Item 0 (New)"),
             ("00000000001", "Item 1"),
             ("00000000002", "Item 2"),
             ("00000000005", "Item 5")
         ]
-        expected_old = [
+        self.old_items = []
+        actual = find_added_items(self.old_items, self.new_items)
+
+        self.assertEqual(expected, actual)
+
+    def test_added_items_empty_master(self):
+        expected = [
             ["", "00000000000", "Item 0 (New)"],
             ["", "00000000001", "Item 1"],
             ["", "00000000002", "Item 2"],
@@ -53,14 +74,22 @@ class TestYoutubeDiff(unittest.TestCase):
         self.old_items = []
         actual = find_added_items(self.old_items, self.new_items)
 
-        self.assertEqual(expected_result, actual)
-        self.assertEqual(expected_old, self.old_items)
+        self.assertEqual(expected, self.old_items)
+
+    #
+    # find_missing_items
+    #
 
     def test_missing_items(self):
-        expected_result = [
+        expected = [
             ("00000000003", "Item 3")
         ]
-        expected_old = [
+        actual = find_missing_items(self.old_items, self.new_items)
+
+        self.assertEqual(expected, actual)
+
+    def test_missing_items_master(self):
+        expected = [
             ["", "00000000000", "Item 0"],
             ["", "00000000001", "Item 1"],
             ["", "00000000002", "Item 2"],
@@ -69,23 +98,36 @@ class TestYoutubeDiff(unittest.TestCase):
         ]
         actual = find_missing_items(self.old_items, self.new_items)
 
-        self.assertEqual(expected_result, actual)
-        self.assertEqual(expected_old, self.old_items)
+        self.assertEqual(expected, self.old_items)
 
     def test_missing_items_empty(self):
-        expected_result = []
-        expected_old = []
+        expected = []
         self.old_items = []
         actual = find_missing_items(self.old_items, self.new_items)
 
-        self.assertEqual(expected_result, actual)
-        self.assertEqual(expected_old, self.old_items)
+        self.assertEqual(expected, self.old_items)
+
+    def test_missing_items_empty_master(self):
+        expected = []
+        self.old_items = []
+        actual = find_missing_items(self.old_items, self.new_items)
+
+        self.assertEqual(expected, actual)
+
+    #
+    # find_renamed_items
+    #
 
     def test_renamed_items(self):
-        expected_result = [
+        expected = [
             ("00000000000", "Item 0", "Item 0 (New)"),
         ]
-        expected_old = [
+        actual = find_renamed_items(self.old_items, self.new_items)
+
+        self.assertEqual(expected, actual)
+
+    def test_renamed_items_master(self):
+        expected = [
             ["", "00000000000", "Item 0"],
             ["", "00000000001", "Item 1"],
             ["", "00000000002", "Item 2"],
@@ -94,17 +136,21 @@ class TestYoutubeDiff(unittest.TestCase):
         ]
         actual = find_renamed_items(self.old_items, self.new_items)
 
-        self.assertEqual(expected_result, actual)
-        self.assertEqual(expected_old, self.old_items)
+        self.assertEqual(expected, self.old_items)
 
     def test_renamed_items_empty(self):
-        expected_result = []
-        expected_old = []
+        expected = []
         self.old_items = []
         actual = find_renamed_items(self.old_items, self.new_items)
 
-        self.assertEqual(expected_result, actual)
-        self.assertEqual(expected_old, self.old_items)
+        self.assertEqual(expected, actual)
+
+    def test_renamed_items_empty_master(self):
+        expected = []
+        self.old_items = []
+        actual = find_renamed_items(self.old_items, self.new_items)
+
+        self.assertEqual(expected, self.old_items)
 
 if __name__=='__main__':
     unittest.main()
