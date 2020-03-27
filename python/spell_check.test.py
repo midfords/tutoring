4627d124461f18ea49e4621d070b52460472c4a1
Sean Midford <seanmidford@outlook.com>
2020-03-27 16:30:07 -0400

    Repaired spell check test file.

--- python/spell_check.test.py
+++ python/spell_check.test.py
@@ -11,30 +11,33 @@ import unittest
 
 from spell_check import spell_check
 
+
 class TestSpellCheck(unittest.TestCase):
 
     def setUp(self):
-        path = os.path.dirname(__file__)
+        path = os.path.dirname(os.path.abspath(__file__))
+        words_path = os.path.join(path, 'words.txt')
 
-        with open(path + '/words.text') as f:
+        with open(words_path, 'rb') as f:
             self.words = set(f.readlines())
 
     def test_valid(self):
         expected = 0
         actual = spell_check("hello world", self.words)
-        
+
         self.assertEqual(expected, actual)
 
     def test_invalid(self):
         expected = 2
         actual = spell_check("hlelo wrold", self.words)
-        
+
         self.assertEqual(expected, actual)
 
     def test_random(self):
         expected = 5
-        actual = spell_check("this is a lnog sentnence and it hhas many mispellled wrods", self.words)
-        
+        actual = spell_check(
+            "this is a lnog sentnence and it hhas many mispellled wrods", self.words)
+
         self.assertEqual(expected, actual)
 
     def test_empty(self):
@@ -52,5 +55,5 @@ class TestSpellCheck(unittest.TestCase):
         self.assertTrue(runtime < 1.0)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()
