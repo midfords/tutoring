4812711cdd7fb8d0f511605f4e922f3ef6b02a98
Sean Midford <seanmidford@outlook.com>
2020-05-06 15:14:05 -0400

    Added mastermind exercise.

---
+++ python/mastermind.py
@@ -0,0 +1,186 @@
+# 
+# Mastermind
+#
+# Python tutoring exercise.
+# Sean Midford, 2020
+#
+from typing import List
+from colorama import Fore, Back, Style
+
+def create_secret() -> (int, int, int, int):
+    """This function creates a new 4 piece secret (in the range of [1, 8]).
+    These values must be randomly selected, duplicate values are allowed.
+    Example return: (4, 2, 2, 6)
+
+    Returns
+    -------
+    (int, int, int, int)
+        A secret of 4 values in the range of [1, 8].
+    """
+    pass
+
+def check_for_win(guess: (int, int, int, int), secret: (int, int, int, int), 
+                  turn: int) -> int:
+    """This function checks the game board for a game over state or win state.
+    A win happens if the 4 guess values exactly matches the 4 secret values.
+    A game over happens if the number of turns is >= 10.
+    If neither of these are true, then the game is still in progress.
+        0 -> game is still in progress.
+        1 -> player has won.
+        2 -> game over.
+
+    Parameters
+    ----------
+    guess : (int, int, int, int)
+        The player's guess.
+    secret : (int, int, int, int)
+        The secret values.
+    turn : int
+        The player's current turn number.
+
+    Returns
+    -------
+    int
+        A code for the game win state.
+    """
+    pass
+
+def get_guess() -> (int, int, int, int):
+    """This function gets 4 integers in the range of [1, 8] from the user. 
+    The function should stop the user from picking a value that is not in 
+    the range of [1, 8], or if the user enters in anything other than a 
+    number. 
+
+    Returns
+    -------
+    (int, int, int, int)
+        A tuple of the player's 4 guesses.
+    """
+    pass
+
+def get_result(guess: (int, int, int, int), secret: (int, int, int, int)) -> List[int]:
+    """This function compares the guess to the secret and generates a result. 
+    The result is returned as a list of values in the range [0, 1]. If there
+    is a value in guess that is also in secret, and in the same position, then
+    a '1' is added to the result. If there is a value in guess that is also in
+    secret, but not in the same position, then a '0' is added to the result. 
+    A guess can only be counted once. For example, if the player's guess was:
+        (2, 2, 2, 2)
+    and the secret was:
+        (1, 2, 3, 4)
+    then the result would be [1] and not [1, 0, 0, 0].
+
+    Result summary:
+        1 -> correct value in correct position
+        0 -> correct value in incorrect position
+
+    Parameters
+    ----------
+    guess : (int, int, int, int)
+        The player's guess.
+    secret : (int, int, int, int)
+        The secret values.
+
+    Returns
+    -------
+    List[int]
+        A list of values in range [0, 1] representing the result.
+    """
+    pass
+
+def print_guess(guess: (int, int, int, int)):
+    """This function prints out the player's guess.
+
+    Hint: use the print_piece function.
+
+    Parameters
+    ----------
+    guess : (int, int, int, int)
+        The player's guess.
+    """
+    pass 
+
+def print_result(result: List[int]):
+    """This function prints out the turn result. For each '1' in the list, 
+    print a white piece (colour code 8). For each '0' in the list, print a 
+    black piece (colour code 7).
+
+    Hint: use the print_piece function.
+
+    Parameters
+    ----------
+    result : List[int]
+        A list of values in range [0, 1] representing the result.
+    """
+    pass
+
+def print_final_result(result: int):
+    """This function prints the final mastermind game result. The result can be:
+        1 -> player wins.
+        2 -> game over.
+
+    Parameters
+    ----------
+    result : int
+        The result to be printed.
+    """
+    pass
+
+def print_welcome():
+    """This function prints out a simple welcome message to the player.
+    """
+    pass
+
+def print_colours_menu():
+    """This function prints out the colour menu. The menu options are: 
+        1 -> "Red"
+        2 -> "Green"
+        3 -> "Blue"
+        4 -> "Yellow"
+        5 -> "Magenta"
+        6 -> "Cyan"
+        7 -> "Black"
+        8 -> "White"
+
+    Hint: use the print_piece function.
+    """
+    pass
+
+def print_piece(colour: int, end: str = ""):
+    colours = [
+        (Fore.RED, Back.BLACK), (Fore.GREEN, Back.BLACK),
+        (Fore.BLUE, Back.BLACK), (Fore.YELLOW, Back.BLACK),
+        (Fore.MAGENTA, Back.BLACK), (Fore.CYAN, Back.BLACK),
+        (Fore.BLACK, Back.WHITE), (Fore.WHITE, Back.BLACK) ]
+
+    assert(colour > 0 and colour <= len(colours))
+
+    i = colour - 1
+    print(
+        f"{colours[i][0]}{colours[i][1]}", "⬤ ", 
+        f"{Style.RESET_ALL}", " ", end, sep="")
+
+def play_game():
+    secret = create_secret()
+
+    turn = 1
+    while 1:
+        print_colours_menu()
+        guess = get_guess()
+        print_guess(guess)
+        result = get_result(guess, secret)
+        print_result(result)
+
+        result = check_for_win(guess, secret, turn)
+        if result != 0:
+            print_final_result(result)
+            break
+
+        turn += 1
+
+def main():
+    print_welcome()
+    play_game()
+
+if __name__ == "__main__":
+    main()
---
+++ python/mastermind.test.py
@@ -0,0 +1,203 @@
+#
+# Mastermind Tests
+#
+# Python tutoring exercise test cases.
+# Sean Midford, 2020
+#
+import unittest
+from io import StringIO
+from unittest.mock import patch
+
+import mastermind as m
+
+class TestMastermind(unittest.TestCase):
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_print_welcome(self, mock_stdout):
+        m.print_welcome()
+        actual = mock_stdout.getvalue()
+
+        self.assertGreater(len(actual), 0)
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_print_colours_menu(self, mock_stdout):
+        m.print_colours_menu()
+        actual = mock_stdout.getvalue()
+
+        self.assertGreater(len(actual), 0)
+
+        for i in range(1, 9):
+            self.assertIn(str(i), actual)
+
+        self.assertIn("\x1b[32m", actual)
+        self.assertIn("\x1b[31m", actual)
+        self.assertIn("\x1b[34m", actual)
+        self.assertIn("\x1b[33m", actual)
+        self.assertIn("\x1b[35m", actual)
+        self.assertIn("\x1b[36m", actual)
+        self.assertIn("\x1b[30m", actual)
+        self.assertIn("\x1b[37m", actual)
+
+    def __call_print_final_result(self, mock_stdout, result):
+        m.print_final_result(result)
+        actual = mock_stdout.getvalue()
+        mock_stdout.truncate(0)
+        mock_stdout.seek(0)
+        return actual
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_print_final_result(self, mock_stdout):
+        actual = []
+        actual.append(self.__call_print_final_result(mock_stdout, 1))
+        actual.append(self.__call_print_final_result(mock_stdout, 2))
+
+        self.assertGreater(len(actual[0]), 0)
+        self.assertGreater(len(actual[1]), 0)
+        self.assertEqual(len(set(actual)), len(actual), 
+            'Output did not differ for each final result code.')
+
+    def test_create_secret_len(self):
+        actual = m.create_secret()
+
+        self.assertEqual(len(actual), 4)
+
+    def test_create_secret_range(self):
+        actual = m.create_secret()
+
+        for e in actual:
+            self.assertGreater(e, 0)
+            self.assertLess(e, 9)
+
+    def test_create_secret_random(self):
+        actual = [ m.create_secret() for _ in range(10) ]
+
+        self.assertGreater(len(set(actual)), 1, 
+            'Create secret is likely not returning random results.')
+
+    @patch('builtins.input', side_effect=['1', '2', '3', '4'])
+    def test_get_guess(self, mock_input):
+        expected = (1, 2, 3, 4)
+        actual = m.get_guess()
+
+        self.assertEqual(expected, actual)
+
+    @patch('builtins.input', side_effect=['9', '1', '2', '0', '3', '3'])
+    def test_get_guess_out_of_range(self, mock_input):
+        expected = (1, 2, 3, 3)
+        actual = m.get_guess()
+
+        self.assertEqual(expected, actual)
+
+    @patch('builtins.input', side_effect=['$', '5', '6', '$', '7', '8'])
+    def test_get_guess_invalid(self, mock_input):
+        expected = (5, 6, 7, 8)
+        actual = m.get_guess()
+
+        self.assertEqual(expected, actual)
+
+    def test_check_for_win(self):
+        expected = 1
+        actual = m.check_for_win((1, 2, 3, 4), (1, 2, 3 ,4), 2)
+        
+        self.assertEqual(expected, actual)
+
+    def test_check_for_win_last_turn(self):
+        expected = 1
+        actual = m.check_for_win((1, 2, 3, 4), (1, 2, 3 ,4), 10)
+        
+        self.assertEqual(expected, actual)
+
+    def test_check_for_win_game_over(self):
+        expected = 2
+        actual = m.check_for_win((1, 2, 3, 4), (4, 3, 2 ,1), 10)
+        
+        self.assertEqual(expected, actual)
+
+    def test_check_for_win_in_progress(self):
+        expected = 0
+        actual = m.check_for_win((1, 2, 3, 4), (2, 2, 4 ,4), 8)
+        
+        self.assertEqual(expected, actual)
+
+    def __call_print_guess(self, mock_stdout, guess):
+        m.print_guess(guess)
+        actual = mock_stdout.getvalue()
+        mock_stdout.truncate(0)
+        mock_stdout.seek(0)
+        return actual
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_print_guess(self, mock_stdout):
+        actual = []
+        actual.append(self.__call_print_guess(mock_stdout, (1, 2, 3, 4)))
+        actual.append(self.__call_print_guess(mock_stdout, (5, 6, 7, 8)))
+
+        self.assertGreater(len(actual[0]), 0)
+        self.assertIn("\x1b[31m", actual[0])
+        self.assertIn("\x1b[32m", actual[0])
+        self.assertIn("\x1b[34m", actual[0])
+        self.assertIn("\x1b[33m", actual[0])
+
+        self.assertGreater(len(actual[1]), 0)
+        self.assertIn("\x1b[35m", actual[1])
+        self.assertIn("\x1b[36m", actual[1])
+        self.assertIn("\x1b[30m", actual[1])
+        self.assertIn("\x1b[37m", actual[1])
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_print_result(self, mock_stdout):
+        m.print_result([0, 1, 1])
+        actual = mock_stdout.getvalue()
+
+        self.assertGreater(len(actual), 0)
+        self.assertIn("\x1b[30m", actual[1])
+        self.assertIn("\x1b[37m", actual[1])
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_print_result_white_only(self, mock_stdout):
+        m.print_result([1])
+        actual = mock_stdout.getvalue()
+
+        self.assertGreater(len(actual), 0)
+        self.assertIn("\x1b[37m", actual[1])
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_print_result_empty(self, mock_stdout):
+        m.print_result([])
+        actual = mock_stdout.getvalue()
+
+        self.assertEqual(len(actual), 0)
+
+    def test_get_result_no_matches(self):
+        expected = []
+        actual = m.get_result((5, 6, 7, 8), (1, 2, 3, 4))
+
+        self.assertCountEqual(expected, actual)
+
+    def test_get_result_all_match(self):
+        expected = [1, 1, 1, 1]
+        actual = m.get_result((1, 2, 3, 4), (1, 2, 3, 4))
+
+        self.assertCountEqual(expected, actual)
+
+    def test_get_result_some_match(self):
+        expected = [1, 0, 0]
+        actual = m.get_result((1, 2, 3, 4), (2, 1, 5, 4))
+
+        self.assertCountEqual(expected, actual)
+
+    def test_get_result_duplicate(self):
+        expected = [1]
+        actual = m.get_result((1, 2, 3, 4), (2, 2, 5, 6))
+
+        self.assertCountEqual(expected, actual)
+
+    def test_get_result_duplicate_guess(self):
+        expected = [1]
+        actual = m.get_result((2, 2, 5, 6), (1, 2, 3, 4))
+
+        self.assertCountEqual(expected, actual)
+
+
+if __name__=='__main__':
+    unittest.main()
